e2ad523c66f2f118c7c1fea6201147d31dfd0dd6
Bisheng Huang <hbisheng@126.com>
2016-01-03 12:53:58 +0800

    Add a boss key, call it and hide it easily

--- src/WorkflowRecorder.java
+++ src/WorkflowRecorder.java
@@ -1,32 +1,47 @@
 import javax.swing.*;
 
+import com.melloware.jintellitype.HotkeyListener;
+import com.melloware.jintellitype.JIntellitype;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
- * Todo:
- * 
- * work classification, pause and resume
- * font style customization, font display
- * 
+ * TODO:
+ * 1. (Developed)A boss key, call it and hide it easily. 
+ * 2. Extended features like timers and alarms(<rest> and wake me up in 5 minutes)
+ * 3. Shortcut key like eclipse, convenient for <interrupt>, <flow> and all kinds of labels. 
+ * (Write a compiler for it to recognize labels?)
+ * 4. Font style customization, font display
+ * 5. Interrupt mechanism, nested interrupt. With marks, it would be better for me to classify events.
+ * 6. Work classification as different files, and thus pause and resume
+ * 7. Undo function.
+ * 8. A beautiful UI. Modern and fashion.
+ * BUG:
+ * 1. Sometimes the frame was not brought to the front.
  * @author tsinghuabci02
  */
 public class WorkflowRecorder{
 	
 	private JFrame backgroundFrame = null;
-	private int BACKGROUND_WIDTH = 350;
-	private int BACKGROUND_HEIGHT = 350;
+	private int BACKGROUND_WIDTH = 450;
+	private int BACKGROUND_HEIGHT = 500;
 	private float FONT_SIZE = 30.0f;
 	
 	private JScrollPane jScrollPanel = null;
+	//private JScrollBar jScrollPanelVerticalScrollBar = null;
+	
 	private JTextArea logTextArea = null;
 	
 	private JTextArea inputArea = null;
@@ -37,9 +52,12 @@ public class WorkflowRecorder{
 	
 	String FILE_NAME = "log.txt";
 	
+	private SimpleDateFormat dateStringFormat = new SimpleDateFormat("yyyy.MM.dd");;
+	boolean getFocus = true;
+	
 	public void build() throws FontFormatException, IOException{
 		
-		Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("Fonts\\YaHei Consolas Hybrid.ttf")).deriveFont(15f);
+		Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("Fonts\\YaHei Consolas Hybrid.ttf")).deriveFont(18f);
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("Fonts\\YaHei Consolas Hybrid.ttf")));
 		
@@ -48,13 +66,18 @@ public class WorkflowRecorder{
 		//logTextArea.setFont(logTextArea.getFont().deriveFont(15f));
 		logTextArea.setLineWrap(true);
 		logTextArea.setFont(customFont);
+		logTextArea.setText(readFile(getFileName()));
 		jScrollPanel = new JScrollPane(logTextArea);
+		
+		//jScrollPanelVerticalScrollBar = jScrollPanel.getVerticalScrollBar();
 		
 		inputArea = new JTextArea("", 3, 2);
 		inputArea.setLineWrap(true);
         inputArea.setFont(inputArea.getFont().deriveFont(20f));
         inputArea.setSize(2, 1);
         inputArea.setBackground(Color.LIGHT_GRAY);
+        
+        
         
         SimpleDateFormat sdf = new SimpleDateFormat("[yyyy-MM-dd kk:mm:ss]");
         // Set key mapping
@@ -77,13 +100,16 @@ public class WorkflowRecorder{
 	        		return;
 	        	
 	        	logTimes++;
-	        	String appendText = sdf.format(new Date())+inputArea.getText()+"\n";
-	        	
-	        	writeToFile(FILE_NAME, appendText);
+	        	String appendText = sdf.format(new Date())+inputArea.getText()+"\n";      	
+	        	writeToFile(getFileName(), appendText);
 	        	logTextArea.append(appendText);
+	        	logTextArea.setCaretPosition(logTextArea.getDocument().getLength());
+	        	// Scroll to bottom
+	        	//jScrollPanelVerticalScrollBar.setValue( jScrollPanelVerticalScrollBar.getMaximum() );
 	        	//logTextArea.insert(sdf.format(new Date())+inputArea.getText()+"\n", 0);
 	        	//logTextArea.setCaretPosition(0);
 	        	//System.out.println(backgroundFrame.getSize().toString());
+	        	
 	        	inputArea.setText("");
 	        }
 	    });
@@ -102,19 +128,53 @@ public class WorkflowRecorder{
 	        	if(logTimes < 2) {
 	        		System.exit(0);
 	        	}
-	        	writeToFile(FILE_NAME, "\n");				
+	        	writeToFile(getFileName(), "\n");				
 	        }
 	    });
 		
-		// Set window positions
-		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-	    int x = (int) ((dimension.getWidth() - backgroundFrame.getWidth()) / 2);
-	    int y = (int) ((dimension.getHeight() - backgroundFrame.getHeight()) / 2);
-	    backgroundFrame.setLocation(x, y);
-	    
+	    /**
+	     *  These lines must be put after the construction of backgroundFrame
+	     */
+	    logTextArea.setCaretPosition(logTextArea.getDocument().getLength());
 	    inputArea.requestFocus();
+	    
+	    /**
+	     * Unmovable and unresizable
+	     */
+	    backgroundFrame.setResizable(false);
+	    Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+	    int x = (int) ((dimension.getWidth() - backgroundFrame.getWidth()))/2;
+	    int y = (int) ((dimension.getHeight() - backgroundFrame.getHeight()) - 45)/2;
+	    backgroundFrame.setLocation(x, y);
+	    backgroundFrame.addComponentListener(new ComponentAdapter() {
+	         public void componentMoved(ComponentEvent e) {
+	        	 backgroundFrame.setLocation(x,y);
+	         }
+	      });
+	    
+	    
+	    // Shortcut
+	    JIntellitype.getInstance().registerHotKey(1, JIntellitype.MOD_ALT+JIntellitype.MOD_SHIFT, 'W');  
+	    JIntellitype.getInstance().addHotKeyListener(new HotkeyListener() {
+	        @Override
+	        public void onHotKey(int markCode) {
+	        	if(getFocus) {
+	        		backgroundFrame.setVisible(false);
+	        	} else {
+	        		backgroundFrame.setVisible(true);
+	        		inputArea.requestFocus();
+	        	}
+	        	getFocus = !getFocus;
+	        }
+	      }); 
 	}
 	
+	
+	protected String getFileName() {
+		// Be ready for date change
+		return dateStringFormat.format(new Date())+".txt";
+	}
+
 	protected void writeToFile(String file_name, String text) {
 		FileOutputStream os = null;
 		OutputStreamWriter or = null;
@@ -134,6 +194,29 @@ public class WorkflowRecorder{
 		}	
 	}
 
+	public static String readFile(String fileName) //, String charSet)
+	{
+		StringBuilder wholefile = new StringBuilder();
+		String s;
+		FileInputStream is;
+		InputStreamReader ir;
+		BufferedReader in;
+		try {
+			is = new FileInputStream(fileName);
+			ir = new InputStreamReader(is);//, charSet);
+			in = new BufferedReader(ir);
+			while((s=in.readLine())!= null) {	
+				wholefile.append(s+"\n");
+			}
+			in.close();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception!");
+		}
+		return wholefile.toString();
+	}
+	
 	public static void main(String[] args) throws FontFormatException, IOException{		
 		new WorkflowRecorder().build();
 	}
